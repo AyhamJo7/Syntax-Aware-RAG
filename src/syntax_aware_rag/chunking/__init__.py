fdec50fbada34949dde3c50b31b57df89394d577
AyhamJo7 <mhd.ayham.joumran@studium.uni-hamburg.de>
2025-10-18 00:35:36 +0200

    Fix import ordering in chunking __init__.py

--- src/syntax_aware_rag/chunking/__init__.py
+++ src/syntax_aware_rag/chunking/__init__.py
@@ -1,10 +1,10 @@
 """Chunking module for splitting documents into semantic units."""
 
 from .base import BaseChunker, TokenCounter
+from .types import Chunk, ChunkerConfig, ChunkType, DocumentMetadata
 from .layout_aware_chunker import LayoutAwareChunker
 from .recursive_chunker import RecursiveCharacterChunker
 from .sentence_chunker import SentenceChunker
-from .types import Chunk, ChunkType, ChunkerConfig, DocumentMetadata
 
 __all__ = [
     "Chunk",
